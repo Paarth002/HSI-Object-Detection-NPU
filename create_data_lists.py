90445b5b39a4a1cf9f86530de2ad94c50f1afc9f
Paarth002 <bhoreparth@gmail.com>
2024-04-03 01:49:59 +0530

    Update create_data_lists

--- create_data_lists.py
+++ create_data_lists.py
@@ -1,4 +1,6 @@
 from utils import create_data_lists
 
-if __name__ == '__main__':
-    create_data_lists(voc07_path='/home/yanlb/code/pytorch_code/SPL_object_detection/VOC2007',output_folder='./')
+if __name__ == "__main__":
+    create_data_lists(
+        voc07_path="/kaggle/input/96-channel-data/VOC2007", output_folder="./"
+    )
